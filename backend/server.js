09f81c79b8af8e214f188d0db0baa71a688730d7
ArseniiMuska06 bc2025-7 <arsenmuska45@gmail.com>
2026-06-29 11:30:55 +0300

    Connected MySQL to the web application's backend

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,19 @@
-const express = require("express");
+import express from "express";
+import dotenv from "dotenv";
+import db from "./config/db.js";
+
+dotenv.config();
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("Backend is working");
 });
+
+
 
 app.get("/api/test", (req, res) => {
   res.json({ message: "This is an answer from the backend" });
